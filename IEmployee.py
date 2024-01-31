0ba93dab8fc0f0923a8946c016f5c7255be777f5
Hiba ERRAOUI <125480922+er-hiba@users.noreply.github.com>
2024-01-31 22:34:44 +0100

    :)

--- IEmployee.py
+++ IEmployee.py
@@ -1,9 +1,14 @@
-class IEmployee:
+from abc import ABC, abstractmethod
+
+class IEmployee(ABC):
+    @abstractmethod
     def age(self):
         pass
 
+    @abstractmethod
     def seniority(self):
         pass
 
+    @abstractmethod
     def retirementDate(self, retirementAge):
         pass
