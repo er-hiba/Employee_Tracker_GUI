474e447f00c859a82a1954f28fd0814a7ecd81f6
Hiba ERRAOUI <125480922+er-hiba@users.noreply.github.com>
2024-01-31 22:31:47 +0100

    :)

--- IR.py
+++ IR.py
@@ -1,4 +1,6 @@
-class IR:
+from abc import ABC, abstractmethod
+
+class IR(ABC):
     incomeBrackets = [0, 30001, 50001, 60001, 80001, 180001]
     taxRates = [0, 0.10, 0.20, 0.30, 0.34, 0.38]
 
@@ -9,3 +11,6 @@ class IR:
                 return IR.taxRates[i-1]
         return IR.taxRates[len(IR.taxRates)-1]
 
+    @abstractmethod
+    def my_method():
+        pass
